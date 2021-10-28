ea524181b93b313621c8e39cab3d7766315e6cfb
Trey Williams <fly.tech157@gmail.com>
2021-10-28 11:05:55 -0400

    need star word count

--- word_frequency.py
+++ word_frequency.py
@@ -7,7 +7,27 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file) as text:
+        text_string = str(text.readlines())
+        text_string = text_string.replace(",", "")
+        text_string = text_string.replace(".", "")
+        text_string = text_string.replace("-", "")
+        text_string = text_string.replace("?", "")
+        text_string = text_string.replace(":", "")
+        text_string = text_string.replace("'", "")
+        text_string = text_string.replace("\\n", "")
+        text_string = text_string.replace("[", "")
+        text_string = text_string.replace("]", "")
+        word_list = text_string.split()
+        no_stop_words = []
+        for word in word_list:
+            if word in STOP_WORDS:
+                pass
+            else: no_stop_words.append(word)
+        clean_list = {}
+        for word in no_stop_words:
+            clean_list[word] = no_stop_words.count(word) 
+        print(clean_list)       
 
 
 if __name__ == "__main__":
